bada301961c7f01b2e6a57555b44ab7c529d810f
Trista <Trista8703@hotmail.com>
2024-02-14 16:58:38 +0200

    added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 console.log("Start Here 😉");
 
-document.getElementById("dark").style.backgroundColour = "black"
+/*const darkColor = document.getElementById("dark");
+const lightColor = document.getElementById("light")
+const crazyColor = document.getElementById("crazy")
+/*const darkColorTest = document.getElementById("boxColor")
+const radios = document.querySelectorAll('input[type="radio"]');
+//darkColor.style.background = "brown"
+
+//box.style.backgroundColor = "grey";
+
+if(darkColor.checked){
+    darkColorTest.style.backgroundColor = "brown"
+}
+
+function changeColor (){
+    if (darkColor){
+        box.style.backgroundColor = "brown"
+    }else if(lightColor){
+        box.style.backgroundColor = "yellow"
+    }else if (crazyColor){
+        box.style.backgroundColor = "purple"
+    }
+}
+
+radios.addEventListener("click", changeColor); */
+const darkColor = document.getElementById("dark")
+const lightColor = document.getElementById("light")
+const crazyColor = document.getElementById("crazy")
+const boxColor = document.getElementsByClassName("box")
+
+function changeColor(event){
+    const chooseColor = event.target.id
+
+    if (chooseColor === darkColor){
+        boxColor.style.backgroundcolor = "brown"
+    }else if(chooseColor === lightColor){
+        boxColor.style.backgroundcolor = "yellow"
+    }else if (chooseColor === crazyColor){
+        boxColor.style.backgroundcolor = "red"
+    }
+}
+
+darkColor.addEventListener("click", changeColor)
+lightColor.addEventListener("click", changeColor)
+crazyColor.addEventListener("click", changeColor)
+
+
+//make 3 different ones, without if-else. because the evenListener is an event ==> action*/
